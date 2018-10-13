c15c390b4318d7f3019d4753d4d05f2105818bc8
Pat Rocchio <procchio6@gmail.com>
2018-10-13 09:05:31 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './sass/index.scss';
+
+const App = () => {
+  return (
+    <div>
+      React Starter App
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
